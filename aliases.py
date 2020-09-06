c7c98a4f339c4596648839b20d489321fbaabd47
Ben Johnson-Staub <benjohnsonstaub@gmail.com>
2020-09-06 15:54:57 -0400

    Cover a bunch of edge cases so we don't get duplicate or nonsensical aliases

--- aliases.py
+++ aliases.py
@@ -15,17 +15,26 @@ class Aliases(commands.Cog):
     async def alias(self, ctx, original_command: str, new_alias: str):
         command = self.bot.get_command(original_command)
         if command is None:
-            await ctx.send(f"{original_command} is not the name of a command, so you cannot add an alias to it")
+            await ctx.send(f"'{original_command}' is not the name of a command, so you cannot add an alias to it")
+            return
+        elif not (self.bot.get_command(new_alias) is None):
+            await ctx.send(f"'{new_alias}' is already a command. You cannot use it as an alias.")
             return
         else:
-            alias_record = Alias()
-            alias_record.server_id = ctx.guild.id
-            alias_record.command = original_command
-            alias_record.alias = new_alias
-            session.add(alias_record)
-            session.commit()
+            existing_alias = session.query(Alias).filter(Alias.alias == new_alias).one_or_none()
+            if existing_alias is None:
+                # We actually create the new alias
+                alias_record = Alias()
+                alias_record.server_id = ctx.guild.id
+                alias_record.command = original_command
+                alias_record.alias = new_alias
+                session.add(alias_record)
+                session.commit()
+                
+                await ctx.send(f"Added alias '{new_alias}' for command '{original_command}'")
+            else:
+                await ctx.send(f"'{new_alias}' already exists as an alias for command '{existing_alias.command}'")
             
-            await ctx.send(f"Added alias '{new_alias}' for command '{original_command}'")
 
     @commands.command()
     @mod_only()
