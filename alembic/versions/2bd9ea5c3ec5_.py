bc655250f36d70be34dc50373d04394d6ddbadc4
Ben Johnson-Staub <benjohnsonstaub@gmail.com>
2020-09-06 12:37:06 -0400

    Add Aliases table to db

---
+++ alembic/versions/2bd9ea5c3ec5_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2bd9ea5c3ec5
+Revises: a89fc81526df, d4e3cb486f17
+Create Date: 2020-09-06 12:35:29.877547
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2bd9ea5c3ec5'
+down_revision = ('a89fc81526df', 'd4e3cb486f17')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
